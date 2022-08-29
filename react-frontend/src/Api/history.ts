cb9577a9ae9b5b9821959d70906e4e3da24fb7ff
nabeel-hussain <luxkyshan@gmail.com>
2022-08-29 21:26:36 +0500

    added session for live site

--- react-frontend/src/Api/history.ts
+++ react-frontend/src/Api/history.ts
@@ -1,11 +1,20 @@
 import backend from "./backend";
-
+import config from "../Shared/config";
 export const getHistory = async () => {
+  let result: Array<TranslationHistory> = [];
+  if (config.backend!=="") {
+    result = JSON.parse(localStorage.getItem("history") || "[]");
+    return result;
+  }
   let res = await backend.get("/history");
-  let result = res.data.history as Array<TranslationHistory>;
+  result = res.data.history as Array<TranslationHistory>;
   return result;
 };
 export const clearHistory = async () => {
+  if (config.backend!=="") {
+    localStorage.removeItem("history");
+    return true;
+  }
   let res = await backend.delete("/history");
   let result = res.data.result as boolean;
   return result;
--- react-frontend/src/Api/translate.ts
+++ react-frontend/src/Api/translate.ts
@@ -1,7 +1,12 @@
 import backend from "./backend";
-
+import config from "../Shared/config";
 export const translate = async (translateObj: Translate) => {
   let res = await backend.post("/translate", translateObj);
   let result = res.data.join("\r\n") as string;
+  if (config.backend!=="") {
+    let history: Array<TranslationHistory>  = JSON.parse(localStorage.getItem("history") || "[]");
+    history.unshift({ ...translateObj,response:res.data})
+    localStorage.setItem("history",JSON.stringify(history))
+  }
   return result;
 };

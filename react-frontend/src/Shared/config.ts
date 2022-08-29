e1fdde904841e834e0a0f8a25871f0e462886fc6
nabeel-hussain <luxkyshan@gmail.com>
2022-08-29 16:24:09 +0500

    Update config.ts

--- react-frontend/src/Shared/config.ts
+++ react-frontend/src/Shared/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    backend: import.meta.url.includes("127.0.0.1") ? "https://localhost:44300" : "",
+    backend: import.meta.url.includes("127.0.0.1") ? "https://localhost:44300" : "https://language-tools.azurewebsites.net",
     username: "lang_tools",
     password: "VeDJvcvB0uiQ1Hd"
   };
